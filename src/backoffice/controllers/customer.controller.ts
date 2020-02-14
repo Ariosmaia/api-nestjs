fa068cd13ab09bbcb44eb4becd36298e78a01a97
Allan <ariosmaia@yahoo.com.br>
2020-02-14 12:03:40 -0300

    Rotas

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,19 +1,27 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Post, Put, Delete } from "@nestjs/common";
 
 //nest generate controller customer
-@Controller()
+// localhost:3000/v1/customers
+// O que eu colocar dentro do Controller vai ser o nome da rota
+@Controller('v1/customers')
 export class CustomerController {
+	@Get()
 	get() {
 		return 'Obter os clientes';
 	}
 
+	@Post()
 	post() { 
 		return 'Criar um cliente'; 
 	}
 
+	@Put()
 	put() { 
 		return 'Atualizar um cliente'; 
 	}
 
-	delete() { return 'Remover um cliente'; }
+	@Delete()
+	delete() { 
+		return 'Remover um cliente'; 
+	}
 }
