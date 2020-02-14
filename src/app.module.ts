d954a94f86b39d442e25c3a75bb0c66a4dc80cdd
Allan <ariosmaia@yahoo.com.br>
2020-02-14 17:06:38 -0300

    Conectando ao Mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BackofficeModule } from './backoffice/backoffice.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 // nest generate module backoffice
 @Module({
-  imports: [BackofficeModule],
+  imports: [
+		MongooseModule.forRoot('CONNECTION_STRING'),
+		BackofficeModule
+	],
   controllers: [],
   providers: [],
 })
