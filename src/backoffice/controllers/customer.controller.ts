876523321f65ae991d83fe84b233390c2f8f7716
Allan <ariosmaia@yahoo.com.br>
2020-02-14 15:01:47 -0300

    Tipando o retorno

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Put, Delete, Param, Body } from "@nestjs/common";
 import { Customer } from "../models/customer.model";
+import { Result } from "../models/result.model";
 
 //nest generate controller customer
 // localhost:3000/v1/customers
@@ -8,33 +9,30 @@ import { Customer } from "../models/customer.model";
 export class CustomerController {
 	@Get()
 	get() {
-		return 'Obter os clientes';
+		return new Result(null, true, [], null);
 	}
 
 	// Dentro do metodo Http eu defino o parametro da rota
 	// Dentro do metodo getById tenho que pegar o parametro do http
 	@Get(':document')
 	getById(@Param('document') document: string) {
-		return 'Obter o cliente ' + document;
+		return new Result(null, true, {}, null);
 	}
 
 	// @Body para pegar os dados do que vem na requisão
 	@Post()
 	post(@Body() body: Customer) {
-		return body.name; 
+		return new Result('Cliente criado com sucesso!', true, body, null);
 	}
 
 	// @Body para pegar os dados do que vem na requisão
 	@Put(':document')
-	put(@Param('document') document, @Body() body) { 
-		return {
-			customer: document,
-			data: body,
-		}; 
+	put(@Param('document') document, @Body() body: Customer) { 
+		return new Result('Cliente alterado com sucesso!', true, body, null);
 	}
 
 	@Delete(':document')
 	delete(@Param('document') document: string) { 
-		return 'Remover um cliente'; 
+		return new Result('Cliente removido com sucesso!', true, null, null);
 	}
 }
