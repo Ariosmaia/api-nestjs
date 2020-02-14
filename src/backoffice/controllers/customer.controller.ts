cc126a412f9e09060b90a8f02693817186a25cd3
Allan <ariosmaia@yahoo.com.br>
2020-02-14 12:38:32 -0300

    Parâmetros de Rotas

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete } from "@nestjs/common";
+import { Controller, Get, Post, Put, Delete, Param, Body } from "@nestjs/common";
 
 //nest generate controller customer
 // localhost:3000/v1/customers
@@ -10,17 +10,29 @@ export class CustomerController {
 		return 'Obter os clientes';
 	}
 
+	// Dentro do metodo Http eu defino o parametro da rota
+	// Dentro do metodo getById tenho que pegar o parametro do http
+	@Get(':document')
+	getById(@Param('document') document) {
+		return 'Obter o cliente ' + document;
+	}
+
+	// @Body para pegar os dados do que vem na requisão
 	@Post()
-	post() { 
-		return 'Criar um cliente'; 
+	post(@Body() body) { 
+		return body; 
 	}
 
-	@Put()
-	put() { 
-		return 'Atualizar um cliente'; 
+	// @Body para pegar os dados do que vem na requisão
+	@Put(':document')
+	put(@Param('document') document, @Body() body) { 
+		return {
+			customer: document,
+			data: body,
+		}; 
 	}
 
-	@Delete()
+	@Delete(':document')
 	delete() { 
 		return 'Remover um cliente'; 
 	}
