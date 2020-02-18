92fae9a3c55f91b8ed6f43c01c004824e0d19126
Allan <ariosmaia@yahoo.com.br>
2020-02-18 18:15:16 -0300

    Criando um pedido

--- src/modules/store/store.module.ts
+++ src/modules/store/store.module.ts
@@ -3,12 +3,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { ProductService } from './services/product.service';
 import { ProductController } from './controllers/product.controller';
+import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order-item.entity';
+import { OrderService } from './services/order.service';
+import { OrderItemService } from './services/order-item.service';
+import { OrderController } from './controllers/order.controller';
 
 @Module({
 	imports:[TypeOrmModule.forFeature([
 		Product,
+		Order,
+		OrderItem
 	])],
-	providers: [ProductService],
-	controllers: [ProductController]
+	providers: [
+		ProductService,
+		OrderService,
+		OrderItemService
+	],
+	controllers: [
+		ProductController,
+		OrderController
+	]
 })
 export class StoreModule {}
