16a4f06d47172160a4b8e95545dc47d4d22c6546
Allan <ariosmaia@yahoo.com.br>
2020-02-18 16:29:03 -0300

    Configurando o TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,28 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BackofficeModule } from 'src/modules/backoffice/backoffice.module';
 import { StoreModule } from 'src/modules/store/store.module';
 
 // nest generate module backoffice
 @Module({
-  imports: [
-		MongooseModule.forRoot('mongodb://localhost:27017/petshop', 
-			{useUnifiedTopology: true, useNewUrlParser:true}),
+	imports: [
+		MongooseModule.forRoot('mongodb://localhost:27017/petshop',
+			{ useUnifiedTopology: true, useNewUrlParser: true }),
+		TypeOrmModule.forRoot({
+			type: 'mssql',
+			host: 'ALLAN-DELL',
+			port: 56646,
+			username: 'petshop',
+			password: '150191',
+			database: 'petshop',
+			entities: [__dirname + '/**/*.entity{.ts,.js}'],
+			synchronize: true,
+		}),
 		BackofficeModule,
 		StoreModule
 	],
-  controllers: [],
-  providers: [],
+	controllers: [],
+	providers: [],
 })
-export class AppModule {}
+export class AppModule { }
