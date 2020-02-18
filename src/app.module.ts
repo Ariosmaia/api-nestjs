6dbb2d2601252d78f586a530ae24b6708b259366
Allan <ariosmaia@yahoo.com.br>
2020-02-18 11:10:45 -0300

    Novo Modulo Store

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BackofficeModule } from 'src/modules/backoffice/backoffice.module';
+import { StoreModule } from 'src/modules/store/store.module';
 
 // nest generate module backoffice
 @Module({
   imports: [
 		MongooseModule.forRoot('mongodb://localhost:27017/petshop', 
 			{useUnifiedTopology: true, useNewUrlParser:true}),
-		BackofficeModule
+		BackofficeModule,
+		StoreModule
 	],
   controllers: [],
   providers: [],
