36caf018180fa718abcb6bdf1299343cf21cd63d
Allan <ariosmaia@yahoo.com.br>
2020-02-20 17:58:58 -0300

    DTOs no CQRS

--- src/modules/agenda/controllers/agenda.controller.ts
+++ src/modules/agenda/controllers/agenda.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/shared/guards/auth.guard';
 import { RoomBookService } from '../services/room-book.service';
+import { BookRoomDto } from '../dtos/book-room.dto';
 
 @Controller('v1/rooms')
 export class AgendaController {
     constructor(private readonly service: RoomBookService) { }
 
     @Post()
-    @UseGuards(JwtAuthGuard)
-    async Book(@Body() body: any) {
+    async Book(@Body() body: BookRoomDto) {
 			console.log('AppController:Book - Iniciando a requisição');
 			await this.service.Book(body.customer, body.room)
     }
