f7dcbea51739fa9993590f5496ca682ca954c11d
Allan <ariosmaia@yahoo.com.br>
2020-02-20 18:05:34 -0300

    Refatorando o command

--- src/modules/agenda/controllers/agenda.controller.ts
+++ src/modules/agenda/controllers/agenda.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Req, HttpException, HttpStatus } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/shared/guards/auth.guard';
 import { RoomBookService } from '../services/room-book.service';
 import { BookRoomDto } from '../dtos/book-room.dto';
+import { BookRoomCommand } from '../commands/book-room.command';
+import { Result } from 'src/modules/backoffice/models/result.model';
 
 @Controller('v1/rooms')
 export class AgendaController {
     constructor(private readonly service: RoomBookService) { }
 
-    @Post()
-    async Book(@Body() body: BookRoomDto) {
+		@Post()
+		@UseGuards(JwtAuthGuard)
+    async Book(@Req() request, @Body() model: BookRoomDto) {
 			console.log('AppController:Book - Iniciando a requisição');
-			await this.service.Book(body.customer, body.room)
+			
+			try {
+				const command = new BookRoomCommand(
+					request.user.document, model.roomId, model.date);
+					await this.service.Book(command);
+			} catch (error) {
+				throw new HttpException(new Result(
+					'Não foi possível reservar sua sala', false, null, error),
+					HttpStatus.BAD_REQUEST);
+			}
     }
 
 }
